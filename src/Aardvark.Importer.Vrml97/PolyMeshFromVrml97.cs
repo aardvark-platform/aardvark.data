8e5e1696189c06af19d000beb196a2f67acabd2e
Stefan Maierhofer <sm@stefanmaierhofer.com>
2018-07-10 08:48:03 +0200

    no more internal dependencies for Aardvark.[Data|Importer].*

--- src/Aardvark.Importer.Vrml97/PolyMeshFromVrml97.cs
+++ src/Aardvark.Importer.Vrml97/PolyMeshFromVrml97.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.IO;
-using System.Text;
-using Aardvark.Base;
+﻿using Aardvark.Base;
 using Aardvark.Geometry;
+using Aardvark.Importer.Vrml97;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Aardvark.VRVis
 {
-
     public static class PolyMeshFromVrml97
     {
         [Flags]
